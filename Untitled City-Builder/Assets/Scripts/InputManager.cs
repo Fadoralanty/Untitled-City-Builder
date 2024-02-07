b95fdea9811f084969579f36ca0cc3b4aeaaa544
Julian Lanty <julianemanueltejo@gmail.com>
2024-02-07 02:34:16 -0300

    imported placeholders
    Building placement Working

--- Untitled City-Builder/Assets/Scripts/InputManager.cs
+++ Untitled City-Builder/Assets/Scripts/InputManager.cs
@@ -1,12 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
     [SerializeField] private Camera sceneCamera;
     [SerializeField] private LayerMask placementMask;
     private Vector3 _lastPosition;
+
+    public event Action OnClicked, OnExit;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnClicked?.Invoke();
+        }        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OnExit?.Invoke();
+        }
+    }
+
+    public bool IsPointerOverUI()
+    {
+        return EventSystem.current.IsPointerOverGameObject();
+    }
 
     public Vector3 GetSelectedMapPosition()
     {
--- Untitled City-Builder/Assets/Scripts/PlacementSystem.cs
+++ Untitled City-Builder/Assets/Scripts/PlacementSystem.cs
@@ -10,10 +10,53 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField] private float cellIndicatorElevation=0.01f;
     [SerializeField] private InputManager inputManager;
     [SerializeField] private Grid grid;
-    
+    [SerializeField] private ObjectsDatabaseSO objectsDatabaseSo;
+    [SerializeField] private GameObject gridVisualization;
+    private int selectedObjectID=-1;
+
+    private void Start()
+    {
+        StopPlacementMode();
+    }
+
+    public void StartPlacement(int ID)
+    {
+        StopPlacementMode();
+        selectedObjectID = objectsDatabaseSo.ObjectDataList.FindIndex(data => data.ID == ID);
+        if (selectedObjectID<0)
+        {
+            Debug.LogError($"No Id Found {ID}");
+            return;
+        }
+        gridVisualization.SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceBuilding;
+        inputManager.OnExit += StopPlacementMode;
+    }
+
+    private void StopPlacementMode()
+    {
+        selectedObjectID = -1;
+        
+        gridVisualization.SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceBuilding;
+        inputManager.OnExit -= StopPlacementMode;
+    }
+
+    private void PlaceBuilding()
+    {
+        if (inputManager.IsPointerOverUI()) { return; }
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+
+        GameObject newBuilding = Instantiate(objectsDatabaseSo.ObjectDataList[selectedObjectID].Prefab);
+        newBuilding.transform.position = grid.CellToWorld(gridPosition) + new Vector3(0, cellIndicatorElevation, 0);
+    }
 
     private void Update()
     {
+        if (selectedObjectID < 0) { return; }
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
         
